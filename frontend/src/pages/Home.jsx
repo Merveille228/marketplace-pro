fd3269f31f9a5e32378ef6e8dcc691eb864b7b74
merveille228 <merveilleamevigbe2@gmail.com>
2026-01-27 11:13:49 +0000

    frontent

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Home() {
     return (
         <>
-        <div className="md:py-30 py-20 px-10">Moise's part</div>
+        <div className="md:py-30 py-20 px-10">Moise's part
+            
+        </div>
         </>
     )
 }
